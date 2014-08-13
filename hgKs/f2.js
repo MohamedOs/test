2716df563dac62642eac5b0dbfa1aea71511fc09
MohamedOs <mos4ma@gmail.com>
2014-08-13 15:08:53 +0300

    Update f2.js

--- hgKs/f2.js
+++ hgKs/f2.js
@@ -12,7 +12,7 @@ var arr2 = [];
 var arrImg=[];
 function singlePost(link){
 	activityIndicator.show();
-	var queryIndex2='select * from html where url="http://qatar.dubizzle.com'+link+'" and xpath="//*[@class=\'u-r\']"';
+	var queryIndex2='select * from html where url="http://saudi.dubizzle.com'+link+'" and xpath="//*[@class=\'u-r\']"';
   Titanium.Yahoo.yql(queryIndex2, function(et){
   	//Ti.API.info(et.data);
   	vars2.data=et.data;
