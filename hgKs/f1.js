29b96227a9dcb56192ce7264ecdad213b9400cf1
MohamedOs <mos4ma@gmail.com>
2014-08-13 15:17:15 +0300

    Update f1.js

--- hgKs/f1.js
+++ hgKs/f1.js
@@ -68,15 +68,23 @@ var ds = vars.data;
        
 vars.d1=(getObjects(ds,'class','u-img__img'));  
 vars.d2=(getObjects(ds,'class','d-listing__name u-helper--no-margin'));  
-vars.d3=(getObjects(ds,'class','d-listing__amount'));  
+vars.d3=(getObjects(ds,'class','d-listing__amount'));
+//Ti.API.info(vars.d3[0].p.content);
+var priceVarsP;
 for (var i=0;i<vars.d2.length;i++) {
+if(vars.d3[i]){
+priceVarsP=vars.d3[i].p.content;
+}else{
+priceVarsP='';
+}
+Ti.API.info(priceVarsP);
 arr.push({
-        image:vars.d1[i].src,
-        title:vars.d2[i].title,
-        href:vars.d2[i].href,
-        price:vars.d3[i].p.content
-    });
-}    
+image:vars.d1[i].src,
+title:vars.d2[i].title,
+href:vars.d2[i].href,
+price:priceVarsP
+});
+}
 //Ti.API.info(arr);
     activityIndicatorCatt.hide();
   	//alert("2");
