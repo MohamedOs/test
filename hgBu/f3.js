3f5f2fcd2cab5261b53419660af1c588a1c5efaa
MohamedOs <mos4ma@gmail.com>
2014-08-14 13:38:13 +0200

    Update f3.js

--- hgBu/f3.js
+++ hgBu/f3.js
@@ -7,9 +7,13 @@ borderRadius:7
 });
 var arr3 = [];
 var vars3={};
-function cat(nation,city){
+function cat(nation,city,eindex){
 activityIndicatorCat2.show();
+if(eindex==0){
+var queryIndexCat='select * from html where url="http://'+nation+'.dubizzle.com/ar/'+city+'/property-for-rent/search/" and xpath="//*[@class=\'d-nav__cat d-nav__cat--show\']"';
+}else{
 var queryIndexCat='select * from html where url="http://'+nation+'.dubizzle.com/ar/'+city+'/property-for-sale/search/" and xpath="//*[@class=\'d-nav__cat d-nav__cat--show\']"';
+}
 Titanium.Yahoo.yql(queryIndexCat, function(etCat){
 // Ti.API.info(etCat.data);
 vars3.data=etCat.data;
