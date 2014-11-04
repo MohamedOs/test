2587f63ab1256d4ccf44deb11f181e2363599c28
MohamedOs <mos4ma@gmail.com>
2014-11-04 21:30:04 +0200

    Update index.js

--- hgOm/index.js
+++ hgOm/index.js
@@ -11,38 +11,29 @@ borderRadius:7
 var vars2={};
 var arr2 = [];
 var arrImg=[];
+var arrContent=[];
 function singlePost(link){
 var contentVars="";
 var adInfoVars="";
 activityIndicator.show();
 
-
-
-
 var xhr = Titanium.Network.createHTTPClient();
 xhr.onload = function() {
 	 
 var select = soupselect.select;
- /*
-var body = '<html><head><title>Test</title></head>'
-		 + '<body>'
-		 + '<img src="http://l.yimg.com/mq/i/home/HKGallery.gif" />'
-		 + '<div id="block">'
-		 + '	<div class="row">Row 1</div>'
-		 + '	<div class="row">Row 2</div>'
-		 + '</div>'
-		 + '</body></html>';
- */
+
 var body=xhr.responseText;
 var handler = new htmlparser.DefaultHandler(function(err, dom) {
 	if (err) {
 		alert('Error: ' + err);
 	} else {
+		vars2.data=select(dom,'div.u-c.u-c--12o12.u-helper--margin-v-large div p');
 		var image,content,phone,content2,content3;
 		image=select(dom,'.rsImg');
 		//Ti.API.info(image);
 		for(var i=0;i<image.length;i++){
-			Ti.API.info(image[i].attribs.src);
+			arrImg.push(image[i].attribs.src);
+		//	Ti.API.info(image[i].attribs.src);
 		}
 		content=select(dom,'div.u-c.u-c--12o12.u-helper--margin-v-large div p');
 		//Ti.API.info(content[0].children[0].data);
@@ -51,46 +42,16 @@ var handler = new htmlparser.DefaultHandler(function(err, dom) {
 		content2=select(dom,'.u-ml__lbl');
 		content3=select(dom,'.u-ml__val');
 		for(var i=0;i<content2.length;i++){
-			Ti.API.info(content2[i].children[0].data);
-			Ti.API.info(content3[i].children[0].data);
+			arrContent.push(content2[i].children[0].data+" : "+content3[i].children[0].data);
 		}
-		//var photos=select(dom,'img.u-img__img');
-		//Ti.API.info(photos[0].attribs.src);
-		//var titles=select(dom,'a.d-listing__name');
-		//Ti.API.info(titles[0].children[0].data);
-		//var prices=select(dom,'div.d-listing__amount');
-		//Ti.API.info(prices[0].children[0].data);
-		//var hrefs=select(dom,'a.d-listing__name');
-		//Ti.API.info(hrefs[0].attribs.href);
-		//var places=select(dom,'div.d-listing__meta');
-		//Ti.API.info(places[0].children[1].children[0].data+' - '+places[0].children[3].children[0].data);
-		//var dates=select(dom,'span.d-listing__date');
-		//Ti.API.info(dates[0].children[0].data);
-		//var rows=select(dom,'#header');
-		//Ti.API.info(rows[0]);
-		//var rows=select(dom,'div.side');
-		//Ti.API.info(rows);
-		/*var rows=select(dom,'.title');
-		Ti.API.info(rows[0]);
-		Ti.API.info("rows[0]");
-		var rows1=select(dom,'a.title');
-		Ti.API.info(rows1[0]);
-	    Ti.API.info("rows[1]");
-		var rows2=select(dom,'a');
-		Ti.API.info(rows2[0]);*/
-		/*
-		var img = select(dom, 'img');
- 
-		img.forEach(function(img) {
-			alert('src: ' + img.attribs.src);
-		});
-		*/
- /*
-		var rows = select(dom, 'div.cnn_hppersonalhdr');
- 
-		rows.forEach(function(row) {
-			Ti.API.info(row.children[0].data);
-		});*/
+	        arr2.push({
+                image:arrImg,
+                content:content[0].children[0].data,
+                phone:phone[0].children[0].data,
+                content2:arrContent
+                  });
+                Ti.API.info(arr2[0]);
+                activityIndicator.hide();
 	}
 });
  
@@ -98,10 +59,6 @@ var parser = new htmlparser.Parser(handler);
 parser.parseComplete(body);
 
 
-
-
-
-   // Titanium.API.info(xhr.responseText);
 };
 xhr.onerror = function() {
     Titanium.API.info('error');
