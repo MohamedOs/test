842c25425eea4ca6176da0cd0f10640a1613cffe
MohamedOs <mos4ma@gmail.com>
2014-11-04 21:09:20 +0200

    Update index.js

--- hgOm/index.js
+++ hgOm/index.js
@@ -1,5 +1,16 @@
-var vars={};
-vars.xpath1="class=\'d-listing__item";
-function pri(priv){
-	vars.city=priv;
+var activityIndicator = Titanium.UI.createActivityIndicator({
+width:'80dip',
+height:'80dip',
+zIndex:1,
+backgroundColor:'black',
+borderRadius:7
+});
+var vars2={};
+var arr2 = [];
+var arrImg=[];
+function singlePost(link){
+var contentVars="";
+var adInfoVars="";
+activityIndicator.show();
+
 }
